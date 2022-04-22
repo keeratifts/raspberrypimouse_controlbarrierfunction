8a19779be7206a3bffde18a5b1f31977256a657b
koekiki <k.fungtammasan@gmail.com>
2022-04-22 15:33:23 +0900

    edited controller parameters

--- scripts/controller.py
+++ scripts/controller.py
@@ -2,9 +2,22 @@
 import numpy as np
 from cvxopt import matrix, sparse
 from cvxopt.solvers import qp, options
+from scipy.integrate import odeint
 from math import *
 
-def si_position_controller(xi, positions, x_velocity_gain=1, y_velocity_gain=1, velocity_magnitude_limit=0.3):
+#Feedback control parameter for REAL ROBOT
+GOAL_DIST_THRESHOLD=0.08
+K_RO=2
+K_ALPHA=8
+V_CONST=0.25
+
+#Feedback control parameter for SIMULATED ROBOT
+GOAL_DIST_THRESHOLD=0.05
+K_RO=3
+K_ALPHA=13
+V_CONST=0.2
+
+def si_position_controller(xi, positions, x_velocity_gain=1, y_velocity_gain=1, velocity_magnitude_limit=0.5):
     _,N = np.shape(xi)
     dxi = np.zeros((2, N))
 
@@ -20,7 +33,7 @@ def si_position_controller(xi, positions, x_velocity_gain=1, y_velocity_gain=1, 
 
     return dxi
 
-def si_barrier_cert(dxi, x, barrier_gain=80, safety_radius=0.25, magnitude_limit=0.1):
+def si_barrier_cert(dxi, x, barrier_gain=80, safety_radius=0.12, magnitude_limit=0.25):
     N = dxi.shape[1]
     num_constraints = int(comb(N, 2))
     A = np.zeros((num_constraints, 2*N))
@@ -49,7 +62,8 @@ def si_barrier_cert(dxi, x, barrier_gain=80, safety_radius=0.25, magnitude_limit
 
     return np.reshape(result, (2, -1), order='F')
 
-def robotFeedbackControl(xi, positions, GOAL_DIST_THRESHOLD=0.05, K_RO=3, K_ALPHA=13, V_CONST=0.2):
+def robotFeedbackControl(xi, positions, GOAL_DIST_THRESHOLD, K_RO, K_ALPHA, V_CONST): #P controller
+    
     _,N = np.shape(xi)
     dxi = np.zeros((2, N))
 
@@ -66,7 +80,7 @@ def robotFeedbackControl(xi, positions, GOAL_DIST_THRESHOLD=0.05, K_RO=3, K_ALPH
     dxi[:, idxs] = 0
     return dxi
 
-def robotPDFeedbackControl(xi, positions, n, a, GOAL_DIST_THRESHOLD=0.05, K_RO=3, K_ALPHA=13, Kd_RO = 5, Kd_ALPHA = 5, V_CONST=0.3):
+def robotPDFeedbackControl(xi, positions, n, a, GOAL_DIST_THRESHOLD=0.05, K_RO=3, K_ALPHA=13, Kd_RO = 5, Kd_ALPHA = 5, V_CONST=0.3): #PD controller
     _,N = np.shape(xi)
     dxi = np.zeros((2, N))
 
@@ -84,3 +98,44 @@ def robotPDFeedbackControl(xi, positions, n, a, GOAL_DIST_THRESHOLD=0.05, K_RO=3
     idxs = np.where(norms < GOAL_DIST_THRESHOLD)
     dxi[:, idxs] = 0
     return dxi, n, a
+
+def diff_equation_fatii(y_list,t,e,omega):
+    ki = 22
+    sum_fu = y_list[1] + e
+    coe = 0
+    for i in range(2,len(y_list)):
+        if i%2 == 0:
+            coe = int(i/2)
+            sum_fu += (y_list[i] + e*cos(coe*omega*t)) * cos(coe*omega*t)
+        else:
+            coe = int((i-1)/2)
+            sum_fu += (y_list[i] + e*sin(coe*omega*t)) * sin(coe*omega*t)
+
+        result = []
+        result.append(-ki*e-sum_fu + 20*cos(pi*t))
+        result.append(-e)
+    for i in range(2, len(y_list)):
+        if i%2 == 0:
+            coe = int(i/2)
+            result.append(coe*e*omega*sin(coe*omega*t) + ki*e*cos(coe*omega*t))
+        else:
+            coe = int((i-1)/2)
+            result.append((-e)*coe*omega*cos(coe*omega*t)+ki*e*sin(coe*omega*t))
+    return np.array(result)
+                     
+
+def cal_tra_fatii(new_coords,new_centroids):
+    T=5
+    t=np.linspace(0,T,num=1)
+    omega = pi*2/T
+    point_lists = []
+    for i in range(len(new_coords)):
+        y_list_x = [new_coords[i][0],0,0,0,0,0,0,0,0,0,0]
+        y_list_y = [new_coords[i][1],0,0,0,0,0,0,0,0,0,0]
+        result_x = odeint(diff_equation_fatii, y_list_x, t, args=(new_coords[i][0]-new_centroids[i][0],omega))
+        result_y = odeint(diff_equation_fatii, y_list_y, t, args=(new_coords[i][1]-new_centroids[i][1],omega))
+        result_xt = result_x[:,0]
+        result_yt = result_y[:,0]
+        new_result = np.vstack((np.array(result_xt), np.array(result_yt))).T
+        point_lists.append(list(new_result))
+    return point_lists
