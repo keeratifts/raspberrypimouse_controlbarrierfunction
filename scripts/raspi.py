60439569ad0c6670a5332529820c8e3bc8d9d9c8
koekiki <k.fungtammasan@gmail.com>
2022-09-06 15:57:51 +0900

    Raspi

--- scripts/raspi.py
+++ scripts/raspi.py
@@ -21,43 +21,51 @@ def getposition(N): #get position in Gazebo simulator.
     return pose
 
 def get_robot_position(N): #get position of ARtag markers attached on the robots using Intel Realsense Camera
-    pose = np.zeros((4, N))
-    i = 0
-    AlvarMsg = rospy.wait_for_message('/ar_pose_marker', AlvarMarkers)
-    for m in AlvarMsg.markers:
-        marker_id = m.id
-        if marker_id >= 0:
-            pose[0, i] = m.pose.pose.position.y
-            pose[1, i] = -m.pose.pose.position.z
-            orientation_q = m.pose.pose.orientation
-            orientation_list =  [orientation_q.y, orientation_q.z, orientation_q.x, orientation_q.w]
-            (roll, pitch, yaw) = euler_from_quaternion(orientation_list)
-            pose[2, i] = -yaw
-            pose[3, i] = m.id
-            i += 1
-    ind = np.argsort(pose[3,:])
-    pose = pose[:,ind]
-    return pose[[0,1,2], :]
+    # pose = np.zeros((4, N))
+    # i = 0
     # AlvarMsg = rospy.wait_for_message('/ar_pose_marker', AlvarMarkers)
-    # pose = np.empty((4, N), float)
-
-    # while len(AlvarMsg.markers) == N:
-    #     for m in AlvarMsg.markers:
-    #         pose[0, m.id] = m.pose.pose.position.y #Since the camera is attached on a ceil. TF frame need to be transformed.
-    #         pose[1, m.id] = -m.pose.pose.position.z
+    # for m in AlvarMsg.markers:
+    #     marker_id = m.id
+    #     if marker_id >= 0:
+    #         pose[0, i] = m.pose.pose.position.y
+    #         pose[1, i] = -m.pose.pose.position.z
     #         orientation_q = m.pose.pose.orientation
     #         orientation_list =  [orientation_q.y, orientation_q.z, orientation_q.x, orientation_q.w]
     #         (roll, pitch, yaw) = euler_from_quaternion(orientation_list)
-    #         pose[2, m.id] = -yaw
-    #         pose[3, m.id] = m.id
+    #         pose[2, i] = -yaw
+    #         pose[3, i] = m.id
+    #         i += 1
+    # ind = np.argsort(pose[3,:])
+    # pose = pose[:,ind]
+    # return pose[[0,1,2], :]
 
-    #     return pose[[0,1,2], :]
+    AlvarMsg = rospy.wait_for_message('/ar_pose_marker', AlvarMarkers)
+    pose = np.empty((4, N), float)
+
+    while True:
+        if len(AlvarMsg.markers) == N:
+            for m in AlvarMsg.markers:
+                pose[0, m.id] = m.pose.pose.position.y #Since the camera is attached on a ceil. TF frame need to be transformed.
+                pose[1, m.id] = -m.pose.pose.position.z
+                orientation_q = m.pose.pose.orientation
+                orientation_list =  [orientation_q.y, orientation_q.z, orientation_q.x, orientation_q.w]
+                (roll, pitch, yaw) = euler_from_quaternion(orientation_list)
+                pose[2, m.id] = -yaw
+                pose[3, m.id] = m.id
+            break
+        else:
+            for m in AlvarMsg.markers:
+                print (m.id)
+            AlvarMsg = rospy.wait_for_message('/ar_pose_marker', AlvarMarkers)
+    ind = np.argsort(pose[3,:])
+    pose = pose[:,ind]
+    return pose[[0,1,2], :]
 
         
 def put_velocities(N, dxu):
     for i in range(0, N):
         velPub = rospy.Publisher('raspi_'+str(i)+'/cmd_vel', Twist, queue_size=3)
-        velMsg = create_vel_msg(np.round(dxu[0, i], 2), np.round(dxu[1, i], 2))
+        velMsg = create_vel_msg(dxu[0, i], dxu[1, i])
         velPub.publish(velMsg)
 
 def create_vel_msg(v, w):
@@ -70,7 +78,7 @@ def create_vel_msg(v, w):
     velMsg.angular.z = w
     return velMsg
 
-def set_velocities(ids, velocities, max_linear_velocity = 0.1, max_angular_velocity = 4):
+def set_velocities(ids, velocities, max_linear_velocity = 0.3, max_angular_velocity = 6):
 
         # Threshold linear velocities
     idxs = np.where(np.abs(velocities[0, :]) > max_linear_velocity)
